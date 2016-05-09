2bcfc017e5d2dff3b7e0d2183c5104d4dbfea9dd
Will Storey <will@summercat.com>
2016-05-09 11:19:38 -0700

    Able to build and install whole site
    Still basic and no pagination yet

--- gallery.go
+++ gallery.go
@@ -11,6 +11,8 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -38,6 +40,7 @@ func (i image) String() string {
 	return fmt.Sprintf("Filename: %s Description: %s Tags: %v", i.filename, i.description, i.tags)
 }
 
+// hasTag checks if the image has the given tag.
 func (i image) hasTag(tag string) bool {
 	for _, myTag := range i.tags {
 		if myTag == tag {
@@ -54,22 +57,49 @@ func (i image) hasTag(tag string) bool {
 // suffix).
 // For thumbnails, 4% looks good for the images I am working
 // with. For regular size, 20% looks OK.
-func (i image) shrink(percent int, dir string) error {
-	namePieces := strings.Split(i.filename, ".")
-	if len(namePieces) != 2 {
-		return fmt.Errorf("Unexpected filename format")
+func (i image) shrink(percent int, imageDir string, thumbsDir string) error {
+	newFilename, err := i.getResizedFilename(percent, thumbsDir)
+	if err != nil {
+		return fmt.Errorf("Unable to determine path to file: %s", err.Error())
 	}
 
-	newFilename := fmt.Sprintf("%s/%s_%d.%s", dir, namePieces[0], percent, namePieces[1])
+	// If the file is already present then there is nothing to
+	// do.
+	_, err = os.Stat(newFilename)
+	if err == nil {
+		return nil
+	}
 
-	cmd := exec.Command("convert", "-resize", fmt.Sprintf("%d%%", pecent), i.filename, newFilename)
+	if !os.IsNotExist(err) {
+		return fmt.Errorf("Problem stat'ing file: %s", err.Error())
+	}
 
-	err := cmd.Run()
+	origFilename := fmt.Sprintf("%s%c%s", imageDir, os.PathSeparator, i.filename)
+
+	log.Printf("Shrinking %s to %d%%...", i.filename, percent)
+
+	cmd := exec.Command("convert", "-resize", fmt.Sprintf("%d%%", percent), origFilename, newFilename)
+
+	err = cmd.Run()
 	if err != nil {
 		return fmt.Errorf("Unable to run command: %s", err.Error())
 	}
 
 	return nil
+}
+
+// getResizedFilename gets the filename and path to the file
+// with the given shrunk size.
+func (i image) getResizedFilename(percent int, thumbsDir string) (string, error) {
+	namePieces := strings.Split(i.filename, ".")
+
+	if len(namePieces) != 2 {
+		return "", fmt.Errorf("Unexpected filename format")
+	}
+
+	newFilename := fmt.Sprintf("%s%c%s_%d.%s", thumbsDir, os.PathSeparator, namePieces[0], percent, namePieces[1])
+
+	return newFilename, nil
 }
 
 func main() {
@@ -109,7 +139,17 @@ func main() {
 	}
 
 	// Generate HTML with chosen images
+	pages, err := generateHTML(chosenImages, myArgs.thumbsDir, myArgs.installDir)
+	if err != nil {
+		log.Fatalf("Problem generating HTML: %s", err.Error())
+	}
+	log.Printf("Generated %d pages", len(pages))
+
 	// Copy images and thumbnails to install directory
+	err = installImages(chosenImages, myArgs.thumbsDir, myArgs.installDir)
+	if err != nil {
+		log.Fatalf("Unable to install images: %s", err.Error())
+	}
 
 	log.Printf("Done!")
 }
@@ -281,4 +321,164 @@ func chooseImages(tags []string, images []image) ([]image, error) {
 // We place the resized images in the thumbs directory.
 // We only resize if the resized image is not already present.
 func generateImages(imageDir string, thumbsDir string, images []image) error {
+	for _, image := range images {
+		err := image.shrink(4, imageDir, thumbsDir)
+		if err != nil {
+			return fmt.Errorf("Unable to resize to %d%%: %s", 4, err.Error())
+		}
+
+		err = image.shrink(20, imageDir, thumbsDir)
+		if err != nil {
+			return fmt.Errorf("Unable to resize to %d%%: %s", 20, err.Error())
+		}
+	}
+
+	return nil
+}
+
+// generateHTML does just that!
+//
+// Split over several pages if necessary.
+func generateHTML(images []image, thumbsDir string, installDir string) ([]string, error) {
+	const tpl = `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Gallery</title>
+</head>
+<body>
+{{range .Images}}
+<div class="image">
+	<a href="{{.Full}}">
+		<img src="{{.Thumb}}">
+	</a>
+</div><-- .image -->
+{{end}}
+</body>
+</html>
+`
+
+	t, err := template.New("page").Parse(tpl)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse HTML template: %s", err.Error())
+	}
+
+	type Image struct {
+		Full  string
+		Thumb string
+	}
+
+	var htmlImages []Image
+	for _, img := range images {
+		thumb, err := img.getResizedFilename(4, thumbsDir)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to determine filename: %s", err.Error())
+		}
+
+		full, err := img.getResizedFilename(20, thumbsDir)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to determine filename: %s", err.Error())
+		}
+
+		htmlImages = append(htmlImages, Image{
+			Full:  basename(full),
+			Thumb: basename(thumb),
+		})
+	}
+
+	data := struct {
+		Images []Image
+	}{
+		Images: htmlImages,
+	}
+
+	htmlPath := fmt.Sprintf("%s%c%s", installDir, os.PathSeparator, "index.html")
+
+	fh, err := os.Create(htmlPath)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to open HTML file: %s", err.Error())
+	}
+	defer fh.Close()
+
+	err = t.Execute(fh, data)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to execute template: %s", err.Error())
+	}
+
+	var pages []string
+
+	pages = append(pages, htmlPath)
+
+	return pages, nil
+}
+
+// basename determines the name of the file or directory.
+// All directory information preceding the lowest will
+// be stripped.
+func basename(file string) string {
+	i := strings.LastIndexByte(file, os.PathSeparator)
+	if i == -1 {
+		return file
+	}
+
+	if i+1 == len(file) {
+		return file
+	}
+
+	return file[i+1:]
+}
+
+// installImages copies the chosen images from the thumbs
+// directory into the install directory.
+func installImages(images []image, thumbsDir string, installDir string) error {
+	for _, image := range images {
+		thumb, err := image.getResizedFilename(4, thumbsDir)
+		if err != nil {
+			return fmt.Errorf("Unable to determine filename: %s", err.Error())
+		}
+
+		full, err := image.getResizedFilename(20, thumbsDir)
+		if err != nil {
+			return fmt.Errorf("Unable to determine filename: %s", err.Error())
+		}
+
+		thumbTarget := fmt.Sprintf("%s%c%s", installDir, os.PathSeparator, basename(thumb))
+
+		fullTarget := fmt.Sprintf("%s%c%s", installDir, os.PathSeparator, basename(full))
+
+		err = copyFile(thumb, thumbTarget)
+		if err != nil {
+			return fmt.Errorf("Unable to copy %s to %s: %s", thumb, thumbTarget, err.Error())
+		}
+
+		err = copyFile(full, fullTarget)
+		if err != nil {
+			return fmt.Errorf("Unable to copy %s to %s: %s", full, fullTarget, err.Error())
+		}
+	}
+
+	return nil
+}
+
+// copyFile copies the file!
+func copyFile(src string, dest string) error {
+	srcFD, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("Unable to open file (read): %s", err.Error())
+	}
+	defer srcFD.Close()
+
+	destFD, err := os.Create(dest)
+	if err != nil {
+		return fmt.Errorf("Unable to open file (write): %s", err.Error())
+	}
+	defer destFD.Close()
+
+	_, err = io.Copy(srcFD, destFD)
+	if err != nil {
+		return fmt.Errorf("Unable to copy file: %s", err.Error())
+	}
+
+	return nil
 }
